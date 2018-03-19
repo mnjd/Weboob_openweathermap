e89f186e81f97f32c29d2edc5ccc0a09e6ee7233
manojd <manoj.ducher@gmail.com>
2018-03-19 13:40:36 +0100

    late commit

---
+++ browser.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+# Copyright(C) 2018      manojd
+#
+# This file is part of weboob.
+#
+# weboob is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# weboob is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with weboob. If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
+
+from weboob.browser import PagesBrowser, URL
+
+from .pages import CityPage, CurrentPage, ForecastPage
+
+__all__ = ['OpenWeatherMapBrowser']
+
+
+class OpenWeatherMapBrowser(PagesBrowser):
+    BASEURL = 'https://openweathermap.org'
+
+    city_page = URL('https://openweathermap.org/data/2.5/find\?q=(?P<pattern>.*)&appid=b6907d289e10d714a6e88b30761fae22', CityPage)
+    current_page = URL('https://openweathermap.org/data/2.5/weather\?id=(?P<city_id>.*)&units=metric&appid=b6907d289e10d714a6e88b30761fae22', CurrentPage)
+    forecast_page = URL('https://openweathermap.org/data/2.5/forecast/daily\?id=(?P<city_id>.*)&units=metric&appid=b6907d289e10d714a6e88b30761fae22', ForecastPage)
+    def iter_city_search(self, pattern):
+        return self.city_page.go(pattern=pattern).iter_cities()
+
+    def get_current(self, city_id):
+        return self.current_page.go(city_id=city_id).get_current()
+
+    def iter_forecast(self, city_id):
+        return self.forecast_page.go(city_id=city_id).iter_forecast()
---
+++ pages.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+# Copyright(C) 2018      manojd
+#
+# This file is part of weboob.
+#
+# weboob is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# weboob is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with weboob. If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+from datetime import date, datetime
+from weboob.browser.elements import ItemElement, method, DictElement, ListElement
+from weboob.browser.filters.standard import Format, Env, CleanText, CleanDecimal, Regexp
+from weboob.browser.pages import HTMLPage, JsonPage
+from weboob.browser.filters.json import Dict
+from weboob.capabilities.weather import Forecast, Current, City, Temperature
+
+
+class CityPage(JsonPage):
+    ENCODING = 'utf-8'
+
+    @method
+    class iter_cities(DictElement):
+            ignore_duplicate = True
+            item_xpath = 'list'
+
+            class itemitem(ListElement):
+                class item(ItemElement):
+                    klass = City
+                    obj_id = Dict['id']
+
+                    def obj_name(self):
+                        return '{}, {} - lat:{}  lon:{}'.format(Dict['name'](self),
+                                           Dict['sys']['country'](self),
+                                           Dict['coord']['lat'](self),
+                                           Dict['coord']['lon'](self))
+
+
+class CurrentPage(JsonPage):
+    @method
+    class get_current(ListElement):
+        ignore_duplicate = True
+
+        class item(ItemElement):
+            klass = Current
+
+            obj_date = date.today()
+            
+            def obj_text(self):
+                return '{} hPa - humidity {}% - {}'.format(Dict['main']['pressure'](self),
+                                                           Dict['main']['humidity'](self),
+                                                           Dict['weather'][0]['description'](self))
+
+            def obj_temp(self):
+                temp = Dict['main']['temp'](self)
+                return Temperature(float(temp), 'C')
+
+
+class ForecastPage(JsonPage):
+    @method
+    class iter_forecast(DictElement):
+        ignore_duplicate = True
+        item_xpath = 'list'
+        
+        class itemitem(ListElement):
+            class item(ItemElement):
+                klass = Forecast
+
+                obj_id = Dict['dt']
+
+                def obj_date(self):
+                    date = Dict['dt'](self)
+                    return datetime.fromtimestamp(int(date)).strftime('%Y-%m-%d')
+
+                def obj_low(self):
+                    temp = Dict['temp']['min'](self)
+                    return Temperature(float(temp), 'C')
+
+                def obj_high(self):
+                    temp = Dict['temp']['max'](self)
+                    return Temperature(float(temp), 'C')
+
+                obj_text = Dict['weather'][0]['description']
