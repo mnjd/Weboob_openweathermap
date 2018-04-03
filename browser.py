859878655d605ec9235286449a3034c53d758ef6
manojd <manoj.ducher@gmail.com>
2018-04-03 15:37:48 +0200

    better handling of url for maintainability

--- browser.py
+++ browser.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright(C) 2018      manojd
+# Copyright(C) 2018      Manoj D
 #
 # This file is part of weboob.
 #
@@ -29,15 +29,18 @@ __all__ = ['OpenWeatherMapBrowser']
 
 class OpenWeatherMapBrowser(PagesBrowser):
     BASEURL = 'https://openweathermap.org'
+    API_KEY = 'b6907d289e10d714a6e88b30761fae22'
 
-    city_page = URL('https://openweathermap.org/data/2.5/find\?q=(?P<pattern>.*)&appid=b6907d289e10d714a6e88b30761fae22', CityPage)
-    current_page = URL('https://openweathermap.org/data/2.5/weather\?id=(?P<city_id>.*)&units=metric&appid=b6907d289e10d714a6e88b30761fae22', CurrentPage)
-    forecast_page = URL('https://openweathermap.org/data/2.5/forecast/daily\?id=(?P<city_id>.*)&units=metric&appid=b6907d289e10d714a6e88b30761fae22', ForecastPage)
+    city_page = URL('data/2.5/find\?q=(?P<pattern>.*)&appid=(?P<api>.*)', CityPage)
+    current_page = URL('data/2.5/weather\?id=(?P<city_id>.*)&units=metric&appid=(?P<api>.*)', CurrentPage)
+    forecast_page = URL('data/2.5/forecast/daily\?id=(?P<city_id>.*)&units=metric&appid=(?P<api>.*)', ForecastPage)
+
+
     def iter_city_search(self, pattern):
-        return self.city_page.go(pattern=pattern).iter_cities()
+        return self.city_page.go(pattern=pattern, api=self.API_KEY).iter_cities()
 
     def get_current(self, city_id):
-        return self.current_page.go(city_id=city_id).get_current()
+        return self.current_page.go(city_id=city_id, api=self.API_KEY).get_current()
 
     def iter_forecast(self, city_id):
-        return self.forecast_page.go(city_id=city_id).iter_forecast()
+        return self.forecast_page.go(city_id=city_id, api=self.API_KEY).iter_forecast()
